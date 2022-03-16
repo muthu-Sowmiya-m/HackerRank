81391ad63c6f31c2b5c2db9e489e3aad83bfdca9
Muthu Sowmiya M <muthusowmya218@gmail.com>
2022-03-16 20:23:13 +0530

    task on 16/3/22

---
+++ BasicProblems/Loops.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Loops {
+
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            int N = Integer.parseInt(bufferedReader.readLine().trim());
+            int i;
+            for(i=1;i<=10;i++)
+            {
+                System.out.println(N + " x " + i + " = " +N*i);
+            }
+            bufferedReader.close();
+        }
+    }
+
